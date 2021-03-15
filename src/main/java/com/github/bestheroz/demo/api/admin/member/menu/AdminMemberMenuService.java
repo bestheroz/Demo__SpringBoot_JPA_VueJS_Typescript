25ebf1635acef861afb2ef62e2d699bd33866d25
joony <joony@kakaopage.com>
2021-03-15 10:47:47 +0900

    회원메뉴관리 에러 수정

--- src/main/java/com/github/bestheroz/demo/api/admin/member/menu/AdminMemberMenuService.java
+++ src/main/java/com/github/bestheroz/demo/api/admin/member/menu/AdminMemberMenuService.java
@@ -1,18 +1,20 @@
 package com.github.bestheroz.demo.api.admin.member.menu;
 
 import com.github.bestheroz.demo.api.entity.member.menu.TableMemberMenuEntity;
-import com.github.bestheroz.demo.api.entity.member.menu.TableMemberMenuEntityId;
 import com.github.bestheroz.demo.api.entity.member.menu.TableMemberMenuRepository;
+import com.github.bestheroz.demo.api.entity.menu.TableMenuRepository;
 import com.github.bestheroz.standard.common.exception.BusinessException;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.annotation.Resource;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AdminMemberMenuService {
   @Resource private TableMemberMenuRepository tableMemberMenuRepository;
+  @Resource private TableMenuRepository tableMenuRepository;
 
   @Transactional
   public List<TableMemberMenuEntity> save(
@@ -22,14 +24,14 @@ public class AdminMemberMenuService {
     return payload.stream()
         .map(
             menu -> {
-              return this.tableMemberMenuRepository
-                  .findById(new TableMemberMenuEntityId(authority, menu.getId()))
+              return this.tableMenuRepository
+                  .findById(menu.getId())
                   .map(
                       (item -> {
-                        item.setAuthority(authority);
-                        item.setId(menu.getId());
-                        this.tableMemberMenuRepository.save(item);
-                        return item;
+                        BeanUtils.copyProperties(item, menu);
+                        menu.setAuthority(authority);
+                        this.tableMemberMenuRepository.save(menu);
+                        return menu;
                       }))
                   .orElseThrow(() -> BusinessException.FAIL_NO_DATA_SUCCESS);
             })
